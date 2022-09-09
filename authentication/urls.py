5a0c3fcd768d747f31c4c0a261d87da7062cf499
Gourab Saha <dreambitsfoundation@users.noreply.github.com>
2022-09-09 17:29:41 +0530

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,7 @@ from authentication.views import LoginView, UserView
 urlpatterns = [
     path('login/', LoginView.as_view(), name='default_user_login'),
     path('user_account/', UserView.as_view(
-        http_method_names=['get', 'post', 'update']), name='user_account_view'),
+        http_method_names=['get', 'post', 'put']), name='user_account_view'),
     path('user_account/<int:pk>/', UserView.as_view(
         http_method_names=['delete']), name='user_account_delete_view'),
 ]
